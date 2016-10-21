875c0689d0da4de90548a6973c32c415f1376c7b
staticsound <johngreedjr@gmail.com>
2016-10-20 21:02:40 -0400

    For now simplify controller

---
+++ public/entry/mainCtrl.js
@@ -0,0 +1,13 @@
+/* global angular */
+(function(angular) {
+  angular.module('headmonControllers').controller('mainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope', 'entryService'];
+
+  function MainCtrl($scope, entry) {
+
+
+
+  }
+
+})(angular);
